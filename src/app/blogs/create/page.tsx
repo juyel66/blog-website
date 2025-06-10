ffb35525b050fd4de571de2318232a1efb7ab1c1
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-06-10 20:06:13 +0600

    banner img change position

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,14 @@
+import BlogForm from '@/components/ui/BlogForm';
+import React from 'react';
+
+const CreateBlogPage = () => {
+    return (
+        <div>
+            <BlogForm></BlogForm>
+
+            
+        </div>
+    );
+};
+
+export default CreateBlogPage;
