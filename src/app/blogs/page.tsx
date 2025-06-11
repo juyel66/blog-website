dc69bd4970f3dc16194c3abf8a64d0e69a961e01
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-06-11 15:59:08 +0600

    redux data fetch

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,17 +1,35 @@
+"use client"
+
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
 import { Blog } from "@/types";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata:Metadata = {
-  title: "NexaBlogs | Blogs"
-}
+// export const metadata:Metadata = {
+//   title: "NexaBlogs | Blogs"
+// }
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store"
-  });
-  const blogs = await res.json();
+const BlogsPage =  () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store"
+  // });
+  // const blogs = await res.json();
+
+  // console.log(blogs);
+
+
+    // data fetch with redux toolkit 
+  const {data:blogs, isLoading} = useGetBlogsQuery({})
+
+  if(isLoading) {
+    return <Spinner></Spinner>
+  }
+ 
+
+  console.log("redux data is:", blogs);
+ 
 
 
 
@@ -28,9 +46,9 @@ const BlogsPage = async () => {
       </p>
 
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-5 ">
-        {
-            blogs.map((blog:Blog) => <BlogCard key={blog.id} blog={blog}></BlogCard>)
-        }
+       {
+            blogs?.map((blog:Blog) => <BlogCard key={blog.id} blog={blog}></BlogCard>)
+        } 
       </div>
     </div>
   );
