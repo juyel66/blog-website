891e352d623b3afe0855843dbbe817a0024fc239
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-06-11 11:25:50 +0600

    api create

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -14,6 +14,24 @@ export const generateStaticParams = async() =>{
     }))
 }
 
+
+// generate MetaData dynamic title name change
+export async function generateMetadata({params}: {params: Promise <{blogId: string}>} ){
+
+     const {blogId} = await params;
+   const res = await fetch(`http://localhost:5000/blogs/${blogId}`)
+   const blog = await res.json();
+
+
+    return{
+        title: blog.title,
+        description: blog.description,
+    }
+}
+
+
+
+
 const BlogDetailsPage = async({params}: {params: Promise <{blogId: string}>} ) => {
    const {blogId} = await params;
    const res = await fetch(`http://localhost:5000/blogs/${blogId}`)
